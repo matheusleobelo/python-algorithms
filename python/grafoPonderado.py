1653b4944694ccedb930c3c3177fdbf2b92b067d
matheusleobelo <bipoladogamer@gmail.com>
2025-06-03 16:50:03 -0300

    feat: algoritmo de grafo ponderado implementado

--- python/grafoPonderado.py
+++ python/grafoPonderado.py
@@ -30,8 +30,35 @@ def acheNoCustoMaisBaixo(custos):
     custoMaisBaixo = float('inf')
     nodoCustoMaisBaixo = None
     for nodo in custos:
-        custo = custos['nodo']
-        if nodoCustoMaisBaixo>custo and nodo not in processados:
+        custo = custos[nodo]
+        if custo < custoMaisBaixo and nodo not in processados:
             custoMaisBaixo = custo
             nodoCustoMaisBaixo = nodo
     return nodoCustoMaisBaixo
+
+nodo = acheNoCustoMaisBaixo(custos)
+while nodo is not None:
+    custo = custos[nodo]
+    vizinhos = grafo[nodo]
+    for n in vizinhos.keys():
+        novo_custo = custo + vizinhos[n]
+        if custos[n] > novo_custo:
+            custos[n] = novo_custo
+            pais[n] = nodo
+    processados.append(nodo)
+    nodo = acheNoCustoMaisBaixo(custos)
+
+chave = 'fim'
+lista = []
+
+for i in range(3):
+    for filho in pais:
+        if filho == chave:
+            lista.insert(0,filho)
+            chave = pais[filho]
+
+print(f'O tempo mínimo é: {custos["fim"]} minutos')
+print(f"Seu caminho é: inicio")
+for i in lista:
+    print(f"Depois: {i}")
+
